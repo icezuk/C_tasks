e1674e894c99cc9bdad274d3ecd1487c041758b1
PlamenHristozov <78652186+icezuk@users.noreply.github.com>
2024-12-17 11:32:11 +0200

    Update print_from_log.c
    Functionality moved out of main

--- Other_tasks/print_from_log.c
+++ Other_tasks/print_from_log.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
-void printInput(char strInput[], int bIsHtml, int bIsPlain)
+void printInput(char strInput[], int iArgTwo)
 {
-  if (bIsHtml == 1)
+  if (iArgTwo == 1)
   {
     int j = 0;
     while((strInput[j] != '\n') && (strInput[j] != '\0'))
@@ -20,84 +21,97 @@ void printInput(char strInput[], int bIsHtml, int bIsPlain)
       printf("%s%s\n", strInput, "<br>");
     }
   }
-  else if(bIsPlain == 1)
+  else if(iArgTwo == 2)
   {
     printf("%s", strInput);
   }
 }
 
-int main(int argc, char * argv[])
+int validateArgTwo(char* argv[], int *iArgTwo)
 {
-  char strInput[300];
-  strInput[300] = '\0';
-  char strMask[100];
-  int iIsBetweenEqual = 0;
-  FILE* pinFP;
-  unsigned long uliPosition;
-  int bIsHtml = 0;
-  int bIsPlain = 0;
-  int i, k = 0;
-  int bHasFoundArgOne = 0;
-  
-  snprintf(strMask, 100, "CLIENT_ID...........: %s", argv[1]);
   if (strcmp(argv[2], "html") == 0)
   {
-    bIsHtml = 1;
+    *iArgTwo = 1;
   }
   else if(strcmp(argv[2], "plain") == 0)
   {
-    bIsPlain = 1;
+    *iArgTwo = 2;
   }
-  if((bIsPlain != 1) && (bIsHtml != 1))
+  if((*iArgTwo != 1) && (*iArgTwo != 2))
   {
     printf("Wrong second argument\n");
-    return -1;
+    exit(-1);
   }
+  return 0;
+}
+
+int inputLogic(char* argv[], int* bHasFoundArgOne, char strMask[], int* iArgTwo, int i)
+{
+  char strInput[300];
+  strInput[300] = '\0';
+  int iIsBetweenEqual = 0;
+  FILE* pinFP;
+  unsigned long uliPosition;
+  int k = 0;
   
+  pinFP = fopen(argv[i], "r");
+  if(pinFP == NULL)
+  {
+    printf("Error file missing\n");
+    exit(-2);
+  }
+  while((feof(pinFP) == 0) && (fgets(strInput, 300, pinFP) != NULL))
+  { 
+    if(strstr(strInput, "============================================================") != NULL)
+    {
+     iIsBetweenEqual = 1;
+     uliPosition = ftell(pinFP);
+    }
+    if(strstr(strInput, strMask) != 0 && iIsBetweenEqual == 1)
+    {
+     *bHasFoundArgOne = 1;
+     fseek(pinFP,uliPosition,SEEK_SET);
+     // strInput is equal to found strMask; put it at start of uliPosition
+     fgets(strInput, 300, pinFP);
+     do
+     {
+      	printInput(strInput, *iArgTwo);
+      } while((fgets(strInput, 300, pinFP) != NULL) && (feof(pinFP) == 0) &&
+              (strstr(strInput,"============================================================") == NULL));
+        
+      if(feof(pinFP) != 0)
+      {
+        printInput(strInput, *iArgTwo);
+      }
+      //ungetc to return the start of next block
+      while(strInput[k] != '\n')
+      {
+      	k++;
+      }
+      for( ; k >= 0; k--)
+      {
+        ungetc(strInput[k], pinFP);
+      }
+      printf("\n\n============================================================\n\n\n");
+      iIsBetweenEqual = 0;
+    }
+  }
+  return 0;
+}
+
+int main(int argc, char * argv[])
+{
+  int iArgTwo = 0;
+  int bHasFoundArgOne = 0;
+  char strMask[100];
+  int i;
+  
+  snprintf(strMask, 100, "CLIENT_ID...........: %s", argv[1]);
+  
+  validateArgTwo(argv, &iArgTwo);
   for(i = 3; i < argc; i++)
   {
-    pinFP = fopen(argv[i], "r");
-    if(pinFP == NULL)
-    {
-      printf("Error file missing\n");
-      return -2;
-    }
-    while((feof(pinFP) == 0) && (fgets(strInput, 300, pinFP) != NULL))
-    { 
-      if(strstr(strInput, "============================================================") != NULL)
-      {
-      	iIsBetweenEqual = 1;
-      	uliPosition = ftell(pinFP);
-      }
-      if(strstr(strInput, strMask) != 0 && iIsBetweenEqual == 1)
-      {
-      	bHasFoundArgOne = 1;
-      	fseek(pinFP,uliPosition,SEEK_SET);
-      	// strInput is equal to found strMask; put it at start of uliPosition
-      	fgets(strInput, 300, pinFP);
-      	do
-      	{
-      	  printInput(strInput, bIsHtml, bIsPlain);
-        } while((fgets(strInput, 300, pinFP) != NULL) && (feof(pinFP) == 0) &&
-                (strstr(strInput,"============================================================") == NULL));
-        
-        if(feof(pinFP) != 0)
-        {
-          printInput(strInput, bIsHtml, bIsPlain);
-        }
-        //ungetc to return the start of next block
-      	while(strInput[k] != '\n')
-      	{
-      	  k++;
-        }
-        for( ; k >= 0; k--)
-        {
-          ungetc(strInput[k], pinFP);
-        }
-      	printf("\n\n============================================================\n\n\n");
-      	iIsBetweenEqual = 0;
-      }
-    }
+    inputLogic(argv, &bHasFoundArgOne, strMask, &iArgTwo, i);
   }
   if(bHasFoundArgOne == 0)
   {
