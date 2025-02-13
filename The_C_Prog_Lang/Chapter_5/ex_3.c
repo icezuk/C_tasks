6abc4baf4cdf253a3f693aaec42157f7dd91a45d
PlamenHristozov <78652186+icezuk@users.noreply.github.com>
2025-02-13 14:50:28 +0200

    Create ex_3.c

---
+++ The_C_Prog_Lang/Chapter_5/ex_3.c
@@ -0,0 +1,21 @@
+//write a pointer version of strcat
+//assume enough space
+#include <stdio.h>
+void strcatt(char *s, char *t)
+{  
+  while(*s++ != '\0')  // find end of s
+    ;
+  s--;
+  while((*s++ = *t++)) //copy t, *s = *t != 0 equiv *s = *t
+    ;
+}
+
+int main()
+{
+  char s1[20] = "Hello, ";
+  char *s2 = "World!";
+  strcatt(s1,s2);
+  printf("%s\n",s1);
+  
+  return 0;
+}
